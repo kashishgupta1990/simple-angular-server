9de929231f72546d2905fdd6c17e5d7036eafa08
deepakshrma <deepak.m.shrma@gmail.com>
2015-04-24 02:20:52 +0530

    added optimist, holy cow.. ;)

--- app.js
+++ app.js
@@ -1,50 +1,31 @@
 #!/usr/bin/env node
 
-// Change FRONT-END PORT here
-var PORT = 3000;
-
-// Change directory "public" here
-var completePath = './';
-var filterPath;
-
-/*Command line arguments
- * Command node app.js [PORT] [PUBLIC-DIR-PATH]*/
-if (process.argv.length > 1) {
-    PORT = (process.argv[2] && !isNaN(process.argv[2]) && +process.argv[2] ) || PORT;
-    completePath = process.argv[3] || completePath;
-}
-
+var argv = require('optimist')
+    .default('port', 3000)
+    .default('dir', '')
+    .argv;
 var express = require('express');
 var currentDir = process.cwd();
-
-if (completePath.match(/\.\//)) {
-    filterPath = completePath.replace(/\.\//i,"");
-    completePath = currentDir + '/' + filterPath;
-}
-
+var path = require('path');
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 
-app.use(express.static(completePath));
-app.set('appPath', completePath);
-
-// Insert routes below
+app.use(express.static(path.resolve(currentDir,argv.dir)));
+app.set('appPath', path.resolve(currentDir,argv.dir));
 
 // All undefined asset or api routes should return a 404
 app.route('/:url(api|auth|components|app|bower_components|assets)/*')
     .get(function (req, res) {
         res.send("Error 404");
     });
-
 // All other routes should redirect to the index.html
 app.route('/*')
     .get(function (req, res) {
         res.sendfile(app.get('appPath') + '/index.html');
     });
-
 // Start server
-server.listen(PORT, function () {
-    console.log('Angular Server PORT: %d ', PORT);
+server.listen(argv.port, function () {
+    console.log('Angular Server PORT: %d ', argv.port);
     console.log('Server Public Directory Path: %s ', app.get('appPath'));
 });
