6b0ccf471ce507e02c682409be6179b6db005699
kashish <kashishgupta1990@yahoo.com>
2015-04-27 20:50:02 +0530

    added cli-color library

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var argv = require('optimist')
     .default('dir', '')
     .default('file', 'index.html')
     .argv;
+
 var express = require('express');
 var currentDir = process.cwd();
 var path = require('path');
+
+var clc = require('cli-color');
+
+var green = clc.green.bold;
+var blue = clc.blue.bold;
+var red = clc.red;
 
 // Setup server
 var app = express();
@@ -31,7 +38,10 @@ app.route('/*').get(function (req, res) {
 
 // Start server
 server.listen(argv.port, function () {
-    console.log('Server Public Directory Path: %s ', app.get('appPath'));
-    console.log('Main Angular File Path: ', path.resolve(app.get('appPath'), argv.file));
-    console.log('Angular Server PORT: %d ', argv.port);
+    console.log('');
+    console.log(red('-------------------- Angular Server Running --------------------'));
+    console.log(blue('Public Directory Path: '), green(app.get('appPath')));
+    console.log(blue('Main Angular File Path: '),green(path.resolve(app.get('appPath'), argv.file)));
+    console.log(blue('Server Listening on PORT: '), green(argv.port));
+    console.log(red('----------------------------------------------------------------'));
 });
